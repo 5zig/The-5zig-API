133a10dd1c9b16435e320525bb64195ab415548c
5zig <alex.albers@me.com>
2016-07-22 18:49:19 +0200

    Better description of NetworkPlayerInfo#setDisplayName(String)

--- src/eu/the5zig/mod/util/NetworkPlayerInfo.java
+++ src/eu/the5zig/mod/util/NetworkPlayerInfo.java
@@ -13,12 +13,14 @@ public interface NetworkPlayerInfo {
 	GameProfile getGameProfile();
 
 	/**
-	 * @return the display name of this player or {@code null}, if no display name has been set.
+	 * @return the display name of this player or {@code null}, if no special display name has been set.
 	 */
 	String getDisplayName();
 
 	/**
-	 * Sets a new display name for this player.
+	 * Sets a new display name for this player. If the unformatted string does not equal {@link GameProfile#getName()} of this player, a yellow star (*) will be added in front of the name.
+	 * <br>
+	 * Setting a display name will also override any scoreboard assigned team color.
 	 *
 	 * @param displayName the new display name for this player.
 	 */
